ca80802ce12be0ff23fb7abbba0ff49a26b1702d
Maxgroene <maxg@tuta.com>
2024-05-29 21:15:24 +0200

    Adding CheckSum filed

--- Bankautomat/SelectMoney.cs
+++ Bankautomat/SelectMoney.cs
@@ -30,77 +30,91 @@ namespace Bankautomat
         {
             tbNumber5.Text = (Convert.ToInt32(tbNumber5.Text) - 1).ToString();
             CheckIfSmallerNull();
+            ChangingLbSum();
         }
 
         private void btMinus10_Click(object sender, EventArgs e)
         {
             tbNumber10.Text = (Convert.ToInt32(tbNumber10.Text) - 1).ToString();
             CheckIfSmallerNull();
+            ChangingLbSum();
         }
 
         private void btMinus20_Click(object sender, EventArgs e)
         {
             tbNumber20.Text = (Convert.ToInt32(tbNumber20.Text) - 1).ToString();
             CheckIfSmallerNull();
+            ChangingLbSum();
         }
 
         private void btMinus50_Click(object sender, EventArgs e)
         {
             tbNumber50.Text = (Convert.ToInt32(tbNumber50.Text) - 1).ToString();
             CheckIfSmallerNull();
+            ChangingLbSum();
         }
 
         private void btMinus100_Click(object sender, EventArgs e)
         {
             tbNumber100.Text = (Convert.ToInt32(tbNumber100.Text) - 1).ToString();
             CheckIfSmallerNull();
+            ChangingLbSum();
         }
 
         private void btMinus200_Click(object sender, EventArgs e)
         {
             tbNumber200.Text = (Convert.ToInt32(tbNumber200.Text) - 1).ToString();
             CheckIfSmallerNull();
+            ChangingLbSum();
         }
 
         private void btMinus500_Click(object sender, EventArgs e)
         {
             tbNumber500.Text = (Convert.ToInt32(tbNumber500.Text) - 1).ToString();
             CheckIfSmallerNull();
+            ChangingLbSum();
         }
 
         private void btPlus5_Click(object sender, EventArgs e)
         {
             tbNumber5.Text = (Convert.ToInt32(tbNumber5.Text) + 1).ToString();
+            ChangingLbSum();
         }
 
         private void btPlus10_Click(object sender, EventArgs e)
         {
             tbNumber10.Text = (Convert.ToInt32(tbNumber10.Text) + 1).ToString();
+            ChangingLbSum();
         }
 
         private void btPlus20_Click(object sender, EventArgs e)
         {
             tbNumber20.Text = (Convert.ToInt32(tbNumber20.Text) + 1).ToString();
+            ChangingLbSum();
         }
 
         private void btPlus50_Click(object sender, EventArgs e)
         {
             tbNumber50.Text = (Convert.ToInt32(tbNumber50.Text) + 1).ToString();
+            ChangingLbSum();
         }
 
         private void btPlus100_Click(object sender, EventArgs e)
         {
             tbNumber100.Text = (Convert.ToInt32(tbNumber100.Text) + 1).ToString();
+            ChangingLbSum();
         }
 
         private void btPlus200_Click(object sender, EventArgs e)
         {
             tbNumber200.Text = (Convert.ToInt32(tbNumber200.Text) + 1).ToString();
+            ChangingLbSum();
         }
 
         private void btPlus500_Click(object sender, EventArgs e)
         {
             tbNumber500.Text = (Convert.ToInt32(tbNumber500.Text) + 1).ToString();
+            ChangingLbSum();
         }
 
         private void tbNumber5_TextChanged(object sender, EventArgs e)
@@ -153,6 +167,14 @@ namespace Bankautomat
             lbFirstName.Text = csv.Customer[CustomerIndex].FirstName;
             lbLastName.Text = csv.Customer[CustomerIndex].LastName;
             lbAccountBalance.Text = "Your Money: " + Convert.ToString(csv.Customer[CustomerIndex].Money) + " €";
+        }
+        void ChangingLbSum()
+        {
+            lbMoneySum.Text = "Sum: " + (Convert.ToInt32(tbNumber5.Text) * 5 + Convert.ToInt32(tbNumber10.Text) * 10 
+                + Convert.ToInt32(tbNumber20.Text) * 20 + Convert.ToInt32(tbNumber50.Text) * 50
+                + Convert.ToInt32(tbNumber100.Text) * 100 + Convert.ToInt32(tbNumber200.Text) * 200
+                + Convert.ToInt32(tbNumber500.Text) * 500).ToString() + " €";
+
         }
         void CheckIfSmallerNull()
         {
