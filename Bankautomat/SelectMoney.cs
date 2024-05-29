7bcb854391c21e76c37ddbf15dfdf68b6b16da74
Maxgroene <maxg@tuta.com>
2024-05-29 20:30:18 +0200

    Adding components to desginer

---
+++ Bankautomat/SelectMoney.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bankautomat
+{
+    public partial class SelectMoney : Form
+    {
+        public SelectMoney()
+        {
+            InitializeComponent();
+        }
+
+        private void btMinus5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btMinus10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btMinus20_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btMinus50_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btMinus100_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btMinus200_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btMinus500_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btPlus5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btPlus10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btPlus20_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btPlus50_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btPlus100_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btPlus200_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btPlus500_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbNumber5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbNumber10_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbNumber20_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbNumber50_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbNumber100_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbNumber200_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbNumber500_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
