bf4556915acbebda7d62d8af71a3583322dda70a
Maxgroene <maxg@tuta.com>
2024-05-29 20:03:19 +0200

    adding the login function

--- Bankautomat/LoginPage.cs
+++ Bankautomat/LoginPage.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -23,14 +24,14 @@ namespace Bankautomat
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            if (string.IsNullOrEmpty(tbIban.Text) && csv.Customers.Count == 0)
+            if (!string.IsNullOrEmpty(tbIban.Text) && csv.Customer.Count != 0)
                 {
 
                 for (int i = 0; i < csv.Customer.Count; i++)
                 {
                     if (csv.Customer[i].Iban == tbIban.Text)
                     {
-
+                        
                     }
                 }
             }
